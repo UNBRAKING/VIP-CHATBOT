892afe91fd1ba2762d239a70f7e751ee9f2c7a01
UNBRAKING THINGS <code.pbail@gmail.com>
2024-10-30 13:35:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "26614080"
+API_ID = "6435225"
 # -------------------------------------------------------------
-API_HASH = "7d2c9a5628814e1430b30a1f0dc0165b"
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("7594693248:AAHubu-qceibM25E03dicbwoxvvppEUWhYA", None)
-MONGO_URL = getenv("mongodb+srv://nsdh77ygz5:b0zdFJkd9frHADgT@cluster0.vvof2.mongodb.net/?retryWrites=true&w=majority", None)
-OWNER_ID = int(getenv("OWNER_ID", "5606990991"))
-SUPPORT_GRP = "Team_Pbail"
-UPDATE_CHNL = "Ps_Support_Group"
-OWNER_USERNAME = "Pbail"
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
+SUPPORT_GRP = "TG_FRIENDSS"
+UPDATE_CHNL = "VIP_CREATORS"
+OWNER_USERNAME = "THE_VIP_BOY"
