76b02822316249f9276a7ead5ad1965bea61eb92
UNBRAKING THINGS <code.pbail@gmail.com>
2024-10-30 15:29:58 +0530

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -48,7 +48,7 @@ EMOJIOS = [
     "🕊",
 ]
 
-BOT = "https://envs.sh/IL_.jpg"
+BOT = "https://i.ibb.co/nf9hH90/Girl-pp.jpg"
 IMG = [
     "https://graph.org/file/210751796ff48991b86a3.jpg",
     "https://graph.org/file/7b4924be4179f70abcf33.jpg",
@@ -120,10 +120,10 @@ async def welcomejej(client, message: Message):
                         chat.photo.big_file_id, file_name=f"chatpp{chat.id}.png"
                     )
                     chat_photo = (
-                        groups_photo if groups_photo else "https://envs.sh/IL_.jpg"
+                        groups_photo if groups_photo else "https://i.ibb.co/nf9hH90/Girl-pp.jpg"
                     )
                 except AttributeError:
-                    chat_photo = "https://envs.sh/IL_.jpg"
+                    chat_photo = "https://i.ibb.co/nf9hH90/Girl-pp.jpg"
                 
                 count = await nexichat.get_chat_members_count(chat.id)
                 chats = len(await get_served_chats())
